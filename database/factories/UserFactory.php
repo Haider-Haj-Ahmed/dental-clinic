5339d860d62a18b5fbf13bfc01fd3b9d186dea58
Haider Al-Haj Ahmed <haideralhajahmed2@gmail.com>
2026-08-28 22:24:00 +0300

    fix(seed): UserFactory.php

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -12,39 +12,42 @@ use Illuminate\Support\Str;
  */
 class UserFactory extends Factory
 {
-    /**
-     * The current password being used by the factory.
-     */
     protected static ?string $password;
 
-    /**
-     * Define the model's default state.
-     *
-     * @return array<string, mixed>
-     */
     public function definition(): array
     {
         return [
-            'name' => fake()->name(),
-            'email' => fake()->unique()->safeEmail(),
-            'role' => fake()->randomElement([
-                User::ROLE_OWNER,
-                User::ROLE_RECEPTIONIST,
-                User::ROLE_PROVIDER,
-            ]),
+            'name'              => fake()->name(),
+            'email'             => fake()->unique()->safeEmail(),
+            'role'              => fake()->randomElement(User::ROLES),
             'email_verified_at' => now(),
-            'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'password'          => static::$password ??= Hash::make('password'),
+            'remember_token'    => Str::random(10),
         ];
     }
 
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
     public function unverified(): static
     {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
+        return $this->state(['email_verified_at' => null]);
+    }
+
+    public function owner(): static
+    {
+        return $this->state(['role' => User::ROLE_OWNER]);
+    }
+
+    public function provider(): static
+    {
+        return $this->state(['role' => User::ROLE_PROVIDER]);
+    }
+
+    public function receptionist(): static
+    {
+        return $this->state(['role' => User::ROLE_RECEPTIONIST]);
+    }
+
+    public function assistant(): static
+    {
+        return $this->state(['role' => User::ROLE_ASSISTANT]);
     }
 }
